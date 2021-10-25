463ae1ba4e153a1dfe2d413a149cebcdf3e5d776
kubPang <773515798@qq.com>
2021-10-25 15:11:17 +0800

    'es'

--- gitee.js
+++ gitee.js
@@ -21,7 +21,7 @@ async function giteeUpdate() {
     await loginButtons[0].click()
     // 6. 等待登录成功
     await page.waitFor(1000)
-    await page.goto('https://gitee.com/kubPang/sourceFile'); // 比如： https://gitee.com/rong0624/blog/pages
+    await page.goto('https://gitee.com/kubPang/sourceFile/pages'); // 比如： https://gitee.com/rong0624/blog/pages
     // 7.1. 监听步骤 7 中触发的确认弹框，并点击确认
     await page.on('dialog', async dialog => {
         console.log('确认更新')
